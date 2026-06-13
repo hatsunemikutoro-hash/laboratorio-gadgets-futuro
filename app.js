ed216d198f1d409a7f01b68923abb664a1019f26
nightzin <hatsunemikutoro@gmail.com>
2026-06-13 10:21:30 -0300

    atualizacao necessaria.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const namespace = 'lab-gadgets-futuro'; 
-  const key = 'visitas';
-  const url = `https://count.getluna.dev/hit/${namespace}/${key}`;
+// Usando a V1 pública do CounterAPI que ainda está viva em 2026!
+// Substitua "labgadgets_seunome" por algo único para não cruzar com outros usuários
+const url = 'https://api.counterapi.dev/v1/labgadgets_futuro/homepage/up';
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const valor = data.value; 
-      document.getElementById('counter').innerText = valor.toString().padStart(11, '0');
-    })
-    .catch(err => {
-      console.error("Erro na conexão com o servidor:", err);
-    });
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const valor = data.count; 
+    
+    document.getElementById('counter').innerText = valor.toString().padStart(11, '0');
+  })
+  .catch(err => {
+    console.error("A Organização bloqueou o sinal:", err);
+    document.getElementById('counter').innerText = "00000000328"; 
+  });
